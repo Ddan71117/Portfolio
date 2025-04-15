70c205f5d2542f4188af81a22026fa040845e41b
Ddan71117 <137979308+Ddan71117@users.noreply.github.com>
2025-04-14 20:27:18 -0600

    Updated deployed link

--- src/Pages/Portfolio.jsx
+++ src/Pages/Portfolio.jsx
@@ -3,7 +3,7 @@ import ProjectCard from "../components/ProjectCard";
 function Portfolio() {
   const projects = [
     { title: "Indexd", bgImg: "bg-card-1", url: "https://github.com/Danr55/Indexd", deployedUrl: "https://danr55.github.io/Indexd/" },
-    { title: "stallStarz", bgImg: "bg-card-2", url: "https://github.com/Ddan71117/stallStarz", deployedUrl: "" },
+    { title: "stallStarz", bgImg: "bg-card-2", url: "https://github.com/Ddan71117/stallStarz", deployedUrl: "https://stallstarz-5q9d.onrender.com" },
     { title: "Flex-Dice", bgImg: "bg-card-3", url: "https://github.com/Ddan71117/Flex-Dice", deployedUrl: "https://flex-dice.vercel.app" },
     { title: "GitHub Candidate Finder", bgImg: "bg-card-4", url: "https://github.com/Ddan71117/GitHub-Candidate-Finder", deployedUrl: "https://github-candidate-finder-b19k.onrender.com" },
     { title: "Quote Generator", bgImg: "bg-card-5", url: "https://github.com/Ddan71117/Quote-Generator", deployedUrl: "https://quote-generator-xi-gold.vercel.app/" },
